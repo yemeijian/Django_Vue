cc35a02a223ad1b270afa003a658254050e4ae9a
yemeijian <yemeijian@hotmail.com>
2021-02-15 23:28:39 +0800

    Initial commit

--- djangoutil/appfront/src/api/api.js
+++ djangoutil/appfront/src/api/api.js
@@ -3,7 +3,11 @@ import axiosInstance from './index'
 const axios = axiosInstance
 
 export const getFundInfo = () => {
-  return axios.get(`http://localhost:8000/api/fundinfo/`)
+  try {
+    return axios.get(`http://localhost:8000/api/fundinfo/`)
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 export const postFundInfo = (fundName, fundCode, isMonitor) => {
--- djangoutil/appfront/src/components/FundInfo.vue
+++ djangoutil/appfront/src/components/FundInfo.vue
@@ -1,7 +1,37 @@
 <template>
   <div class="hello">
     <h1>基金监控信息</h1>
-    <Button @click="loadFundInfo" type="primary" shape="circle" icon="ios-search">查询基金监控信息</Button>
+    <div class="header">
+      <div class="header">
+        <Button type="primary" @click="ModalBulletFrame.modal = true">添加基金监控信息</Button>
+        <Modal
+          v-model="ModalBulletFrame.modal"
+          title="添加基金监控信息"
+          :loading="ModalBulletFrame.loading"
+          @on-ok="asyncOK">
+          <Form ref="formValidate" :model="formFundInfo" :rules="ruleFundInfo" :label-width="100">
+            <FormItem label="基金名字：" prop="formFundName">
+              <Input v-model="formFundInfo.formFundName" placeholder="请输入基金名字"></Input>
+            </FormItem>
+            <FormItem label="基金代码：" prop="formFundCode">
+              <Input v-model="formFundInfo.formFundCode" placeholder="请输入基金代码"></Input>
+            </FormItem>
+            <FormItem label="是否监控" prop="formIsMonitor">
+              <RadioGroup v-model="formFundInfo.formIsMonitor">
+                <Radio label="true">是</Radio>
+                <Radio label="false">否</Radio>
+              </RadioGroup>
+            </FormItem>
+            <FormItem>
+              <Button @click="handleReset('formValidate')" style="margin-left: 8px">清空</Button>
+            </FormItem>
+          </Form>
+        </Modal>
+      </div>
+      <div class="header">
+        <Button @click="loadFundInfo()" type="primary" shape="circle" icon="ios-search">查询基金监控信息</Button>
+      </div>
+    </div>
     <Table :columns="fundInfoListText" :data="fundInfoList"></Table>
   </div>
 </template>
@@ -11,7 +41,6 @@
 import {getFundInfo, postFundInfo} from '../api/api'
 
 export default {
-  el: '#fundinfo',
   name: 'FundInfo',
   data () {
     return {
@@ -27,18 +56,63 @@ export default {
           key: 'isMonitor'
         }
       ],
-      fundInfoList: []
+      formFundInfo: {
+        formFundName: '',
+        formFundCode: '',
+        formIsMonitor: ''
+      },
+      ruleFundInfo: {
+        formFundName: [
+          {required: true, message: '基金名字不能为空', trigger: 'blur'}
+        ],
+        formFundCode: [
+          {required: true, message: '基金代码不能为空', trigger: 'blur'}
+          // {type: 'integer', message: '基金代码无效', trigger: 'blur'}
+        ],
+        formIsMonitor: [
+          {required: true, message: '请选择是否监控', trigger: 'change'}
+        ]
+      },
+      fundInfoList: [],
+      ModalBulletFrame: {
+        modal: false,
+        loading: true
+      }
     }
   },
   methods: {
     loadFundInfo () {
       getFundInfo().then(response => {
-        console.debug(response.data)
-        this.fundInfoList = response.data.results
+        console.log(response.data)
+        if (response.status === 200) {
+          this.fundInfoList = response.data.results
+          this.$Message.success('查询成功')
+        } else {
+          this.$Message.error('查询失败：' + response.data)
+        }
       })
+    },
+    asyncOK () {
+      this.$refs.formValidate.validate((valid) => {
+        if (valid) {
+          this.$Message.success('Success!')
+        } else {
+          this.$Message.error('Fail!')
+        }
+      })
+      setTimeout(() => {
+        this.ModalBulletFrame.modal = false
+        console.log(this.formFundInfo)
+        postFundInfo(this.formFundInfo.formFundName, this.formFundInfo.formFundCode, this.formFundInfo.formIsMonitor).then(response => {
+          console.log(response.data)
+          this.loadFundInfo()
+        })
+      }, 0)
+    },
+    handleReset (name) {
+      this.$refs[name].resetFields()
     }
   }
-
 }
 </script>
 
@@ -47,7 +121,12 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
+
 a {
   color: #42b983;
+}
+
+.header {
+  display: inline;
 }
 </style>
